a3f05dd1c0e283a6dbd0d432dba036aaadde98a1
Robell <robellasfaw@gmail.com>
2025-02-20 13:41:36 +0000

    update to form

--- scripts/script.js
+++ scripts/script.js
@@ -2,38 +2,72 @@ const sectionEl = document.querySelector(".recommended__section")
 const buttonEl = document.querySelector(".recommended__button")
 const containerEl = document.querySelector(".recommended__container")
 const listEl = document.querySelector(".recommended__list")
+const formEl = document.querySelector(".form")
 
 
 
 const recomendedTracks = async () => {
-    const { data } = await axios.get("https://api.reccobeats.com/v1/track/recommendation?size=5&seeds=ec926370-d21b-4640-865e-d2cfd54b3553")
-    const { data1 } = await axios.get("")
-
-    const values = data.content;
-
-    values.forEach((value) => {
-        const recommendedItems = value.artists[0].name;
-        const links = value.artists[0].href
-        console.log(value.artists);
+    try {
+        const { data } = await axios.get(`https://api.reccobeats.com/v1/track/recommendation?size=5&seeds=ec926370-d21b-4640-865e-d2cfd54b3553&danceability=${danceId}&energy=${energyId}&liveness=${liveId}&loudness=${loudId}&valence=${valId}`);
 
 
-        const listEl = document.createElement("ul")
-        listEl.classList.add("recommended__list")
-        containerEl.appendChild(listEl)
+        const dataResponse = data
 
-        const itemEl = document.createElement("li")
-        itemEl.classList.add("recommended__item")
-        listEl.appendChild(itemEl)
+        const values = data.content;
 
-        itemEl.innerText = recommendedItems
+        values.forEach((value) => {
+            const recommendedItems = value.artists[0].name;
+            const links = value.artists[0].href
 
 
 
-    })
+            const listEl = document.createElement("ul")
+            listEl.classList.add("recommended__list")
+            containerEl.appendChild(listEl)
+
+            const itemEl = document.createElement("li")
+            itemEl.classList.add("recommended__item")
+            listEl.appendChild(itemEl)
+
+            itemEl.innerText = recommendedItems
+
+
+
+        })
+    } catch (error) {
+        console.log("error");
+
+    }
+    // const { data } = await axios.get(`https://api.reccobeats.com/v1/track/recommendation?size=5&seeds=ec926370-d21b-4640-865e-d2cfd54b3553&danceability=${}&energy=${}&liveness=${}&loudness=${}&valence=${}`)
+
+
+    // const dataResponse = data
+
+    // const values = data.content;
+
+    // values.forEach((value) => {
+    //     const recommendedItems = value.artists[0].name;
+    //     const links = value.artists[0].href
+
+
+
+    //     const listEl = document.createElement("ul")
+    //     listEl.classList.add("recommended__list")
+    //     containerEl.appendChild(listEl)
+
+    //     const itemEl = document.createElement("li")
+    //     itemEl.classList.add("recommended__item")
+    //     listEl.appendChild(itemEl)
+
+    //     itemEl.innerText = recommendedItems
+
+
+
+    // })
 }
 
 
-buttonEl.addEventListener("click", (event) => {
+formEl.addEventListener("submit", (event) => {
     event.preventDefault()
     recomendedTracks()
 
