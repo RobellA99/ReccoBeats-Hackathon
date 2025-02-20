c0b5aa9286d135b344e1ce7d143b714fe250ff73
Robell <robellasfaw@gmail.com>
2025-02-20 16:10:44 +0000

    Add JSON file to cache Api

---
+++ README.md
@@ -0,0 +1,7 @@
+# ReccoBeats
+
+Hackathon to intearct with Recco Beats Api
+
+## Notes
+
+Api stopped responding at some point, therefore, I decided to cache the responses in `cache/`
--- scripts/script.js
+++ scripts/script.js
@@ -1,77 +1,53 @@
 const sectionEl = document.querySelector(".recommended__section")
 const buttonEl = document.querySelector(".recommended__button")
 const containerEl = document.querySelector(".recommended__container")
-const listEl = document.querySelector(".recommended__list")
+
 const formEl = document.querySelector(".form")
 
 
 
-const recomendedTracks = async () => {
-    try {
-        const { data } = await axios.get(`https://api.reccobeats.com/v1/track/recommendation?size=5&seeds=ec926370-d21b-4640-865e-d2cfd54b3553&danceability=${danceId}&energy=${energyId}&liveness=${liveId}&loudness=${loudId}&valence=${valId}`);
+const recomendedTracks = async (trackId, danceability) => {
+    const { data } = await axios.get(`https://api.reccobeats.com/v1/track/recommendation?size=10&seeds=${trackId}&danceability=${danceability}&energy=0.5&liveness=0.5&loudness=-30&valence=0.5`)
+    // const { data } = await axios.get(`../cache/track-recommendation.json`)
 
 
-        const dataResponse = data
+    const values = data.content;
 
-        const values = data.content;
-
-        values.forEach((value) => {
-            const recommendedItems = value.artists[0].name;
-            const links = value.artists[0].href
+    const listEl = document.querySelector("ul")
+    listEl.innerHTML = "";
 
 
 
-            const listEl = document.createElement("ul")
-            listEl.classList.add("recommended__list")
-            containerEl.appendChild(listEl)
+    values.forEach((value) => {
+        const recommendedItems = value.artists[0].name;
+        const links = value.artists[0].href
 
-            const itemEl = document.createElement("li")
-            itemEl.classList.add("recommended__item")
-            listEl.appendChild(itemEl)
+        const anchorEl = document.createElement("a")
+        anchorEl.classList.add("recommended__link")
+        listEl.appendChild(anchorEl)
 
-            itemEl.innerText = recommendedItems
+        const itemEl = document.createElement("li")
+        itemEl.classList.add("recommended__item")
+        anchorEl.appendChild(itemEl)
 
-
-
-        })
-    } catch (error) {
-        console.log("error");
-
-    }
-    // const { data } = await axios.get(`https://api.reccobeats.com/v1/track/recommendation?size=5&seeds=ec926370-d21b-4640-865e-d2cfd54b3553&danceability=${}&energy=${}&liveness=${}&loudness=${}&valence=${}`)
-
-
-    // const dataResponse = data
-
-    // const values = data.content;
-
-    // values.forEach((value) => {
-    //     const recommendedItems = value.artists[0].name;
-    //     const links = value.artists[0].href
-
-
-
-    //     const listEl = document.createElement("ul")
-    //     listEl.classList.add("recommended__list")
-    //     containerEl.appendChild(listEl)
-
-    //     const itemEl = document.createElement("li")
-    //     itemEl.classList.add("recommended__item")
-    //     listEl.appendChild(itemEl)
-
-    //     itemEl.innerText = recommendedItems
-
-
-
-    // })
+        itemEl.innerText = recommendedItems
+    })
 }
 
 
 formEl.addEventListener("submit", (event) => {
     event.preventDefault()
-    recomendedTracks()
+    const danceability = event.target.dancebility.value;
+
+    const trackId = event.target.trackId.value
+
+    console.log(trackId);
 
 
+    // console.log(danceability);
+
+
+    recomendedTracks(trackId, danceability)
 })
 
 
